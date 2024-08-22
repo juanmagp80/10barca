19c6f2fcada2a6d1dd5a280459d48e0381a215a7
juanmagp80 <juangpdev@gmail.com>
2024-08-23 00:11:17 +0200

    add latest news component

--- src/app/components/NewsCards/NewsCards.jsx
+++ src/app/components/NewsCards/NewsCards.jsx
@@ -20,31 +20,36 @@ const NewsCards = () => {
     }, []);
 
     return (
-        <div className="p-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {articles.map((article, index) => (
-                <div
-                    key={index}
-                    className="bg-white shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-2xl"
-                >
-                    {article.urlToImage && (
-                        <Image
-                            src={article.urlToImage}
-                            alt={article.title}
-                            width={800} // Ajusta el ancho según sea necesario
-                            height={450} // Ajusta la altura según sea necesario
-                            className="w-full h-48 object-cover" // Ajusta el alto de la imagen
-                        />
-                    )}
-                    <div className="p-4">
-                        <h2 className="text-xl font-bold mb-2">{article.title}</h2>
-                        <p className="text-gray-700 mb-4">{article.description}</p>
-                        <Link href={article.url} className="text-blue-500 hover:underline">
-                            Leer más
-                        </Link>
+        <>
+            <h1 className="text-5xl pt-8 text-center font-bold mb-4">Últimas noticias</h1>
+
+            <div className="p-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+
+                {articles.map((article, index) => (
+                    <div
+                        key={index}
+                        className="bg-white shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-2xl"
+                    >
+                        {article.urlToImage && (
+                            <Image
+                                src={article.urlToImage}
+                                alt={article.title}
+                                width={800} // Ajusta el ancho según sea necesario
+                                height={450} // Ajusta la altura según sea necesario
+                                className="w-full h-48 object-cover" // Ajusta el alto de la imagen
+                            />
+                        )}
+                        <div className="p-4">
+                            <h2 className="text-xl font-bold mb-2">{article.title}</h2>
+                            <p className="text-gray-700 mb-4">{article.description}</p>
+                            <Link href={article.url} className="text-blue-500 hover:underline">
+                                Leer más
+                            </Link>
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </>
     );
 };
 
